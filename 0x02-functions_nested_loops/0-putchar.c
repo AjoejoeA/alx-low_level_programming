093bfa97a8608c6bb91808ad2b663002ac2334ed
AjoejoeA <ajoejoe9@gmail.com>
2023-06-20 08:16:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
- #include "main.h"
+#include "main.h"
 
- /**
-  * main – entry point
-  * Return: Always 0
-  */
+/**
+ * main – entry point
+ * Return: Always 0
+ */
 
- int main(void)
+int main(void)
 
- {
-	_putchar('_');
+{
+        _putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
