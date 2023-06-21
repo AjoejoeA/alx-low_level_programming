4a9ccfb6bd14e9c3bc84acdedfc7d9a0d3dcd1e3
AjoejoeA <ajoejoe9@gmail.com>
2023-06-20 17:25:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,50 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
- * @n: The number to start from.
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: Number of the times table.
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	int i;
+    int i, j, k;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
-		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
-		}
-	}
+    if (n >= 0 && n <= 15)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                k = j * i;
+                if (j == 0)
+                {
+                    _putchar(k + '0');
+                }
+                else if (k < 10 && j != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(k + '0');
+                }
+                else if (k >= 10 && k < 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar((k / 10) + '0');
+                    _putchar((k % 10) + '0');
+                }
+                else if (k >= 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((k / 100) + '0');
+                    _putchar(((k / 10) % 10) + '0');
+                    _putchar((k % 10) + '0');
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
