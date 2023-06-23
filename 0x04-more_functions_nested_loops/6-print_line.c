52f9d66cd5a2a7fe16927371019cd1cc2d5d44cb
AjoejoeA <ajoejoe9@gmail.com>
2023-06-23 08:57:25 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,10 @@ void print_line(int n)
 	_putchar('\n');
 	}
 	else
-	int i;
 	{
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-	_putchar('_');
+	_putchar(95);
 	}
 	_putchar('\n');
 	}
